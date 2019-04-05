576cc31983a3a67390929ba131156729b39a5b23
Min-92 <okssud1@naver.com>
2019-04-05 20:45:05 +0900

    array 를 arrayList로 변경
    변경으로 인해 코드 길이 단축

--- step1/convertor.java
+++ step1/convertor.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 
 public class convertor {
-	public static boolean[] dec2bin(int decimal) {
+	public static ArrayList dec2bin(int decimal) {
+		ArrayList<Integer> answer = new ArrayList();
 		int a = decimal;
-		int i = 1;
-		int r;
-		while(a!=1) {
-			i++;
+		int r;	
+		for(int i = 0; a!=1; i++) {
+			r = a%2;
 			a = a/2;
+			answer.add(r);
+			if(a == 1) {
+				answer.add(a);
+			}
 		}
-		
-        boolean[] answer = new boolean[i];
-        answer[i-1] = (1!=0);
-        a = decimal;
-        i = 0;
-        while(a!=1) {
-        	r = a%2;
-        	a = a/2;
-        	answer [i] = (r!=0);
-        	i++;
-        }
         return answer;
     }
-	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);		
 		int a = sc.nextInt();
-		for(int i = 0; i < dec2bin(a).length; i++) {
-			System.out.print((dec2bin(a)[i]? 1:0));
+		
+		ArrayList<Integer> ans = new ArrayList();
+		
+		ans = dec2bin(a);
+		Iterator itr = ans.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.print(itr.next());
 		}
 	}
 
