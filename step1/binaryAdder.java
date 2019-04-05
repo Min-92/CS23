72875af257d3d7d8f75f3f0419c23f098b827757
Min-92 <okssud1@naver.com>
2019-04-05 19:12:16 +0900

    and, or, xor gate 추가, halfadder,fulladder 게이트 함수 호출로 변경
    and or xor 게이트함수를 추가하고 반가산기와 전가산기가 해당
    함수를호출해서 값을 반환하도록 변환

--- step1/binaryAdder.java
+++ step1/binaryAdder.java
@@ -1,21 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class binaryAdder {
 
-	public static boolean[] halfadder(boolean bitA, boolean bitB) {
+	public static boolean andG(boolean a, boolean b) {
+		boolean answer = a&&b;
+		return answer;
+	}
+	public static boolean orG(boolean a, boolean b) {
+		boolean answer = a||b;
+		return answer;
+	}
+	public static boolean xorG(boolean a, boolean b) {
+		boolean answer;
+		
+		answer = orG(andG(!a,b),andG(a,!b));
+		
+		return answer;
+	}
+
+	public static boolean[] halfadder(boolean a, boolean b) {
 	        boolean[] answer = new boolean[2];
-	        answer[0] = bitA&&bitB;                // carry
-	        answer[1] = !bitA&&bitB||bitA&&!bitB;  // sum
+	        
+	        answer[0] = andG(a,b);
+	        answer[1] = xorG(a,b);
 
 	        return answer;
-	    }
-
-	  public static boolean[] fulladder(boolean bitA, boolean bitB, boolean carry) {
+	}
+	
+	public static boolean[] fulladder(boolean a, boolean b, boolean c) {
 	        boolean[] answer = new boolean[2];
-	        answer[0] = bitA&&bitB||bitB&&carry||bitA&&carry ;
-	        answer[1] = bitA&&bitB&&carry||!bitA&&!bitB&&carry||!bitA&&bitB&&!carry||bitA&&!bitB&&!carry;
-
+	        
+	        answer[0] = orG(halfadder(a,b)[0], halfadder(c,halfadder(a,b)[1])[0]);
+	        answer[1] = halfadder(c,halfadder(a,b)[1])[1];
+	        
+	        
 	        return answer;
-	    }
-
+	}
 
 	public static boolean[] byteadder(boolean[] byteA, boolean[] byteB) {
 	        boolean[] answer = new boolean[byteA.length+1];
@@ -44,10 +65,10 @@ public class binaryAdder {
 
 	public static void main(String[] args) {
 
-    ArrayList<Integer> A = new ArrayList<Integer>(
-  				Arrays.asList(1, 1, 0, 0, 1, 0, 1, 0 ));
-  		ArrayList<Integer> B = new ArrayList<Integer>(
-  				Arrays.asList(1, 1, 0, 0, 1, 0, 1, 0 ));
+		ArrayList<Integer> A = new ArrayList<Integer>(
+			Arrays.asList(1, 1, 0, 0, 1, 0, 1, 0 ));
+		ArrayList<Integer> B = new ArrayList<Integer>(
+			Arrays.asList(1, 1, 0, 1, 1, 0, 0, 1 ));
 
   		int length = A.size();
 
