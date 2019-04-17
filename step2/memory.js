227a9fc108513b3f5baea04a52fd1a79c225dafd
Min-92 <okssud1@naver.com>
2019-04-17 19:58:39 +0900

    memory.js 클래스화

--- step2/memory.js
+++ step2/memory.js
@@ -1,60 +1,59 @@
+class Memory {
 
-let prgCnt = 0;
+    constructor() {
+        this.programText = new Uint16Array(0x10000);
+        this.programHeap = new Uint16Array(0x10000);
+        this.programCount = 0;
+    }
 
-let programText = new Array();
-let programHeap = new Array();
+    peek(address) {
+        if (address >= 0 && address <= 0xFFFF) {
 
+            return this.programText[address];
 
-function peek(address){
-    if(address >= 0 && address <= 0xFFFF){
-    
-        return programText[address];
+        } else if (address > 0xFFFF && address <= 0x1FFFF) {
 
-    }else if(address > 0xFFFF && address <= 0x1FFFF){
-    
-        return programHeap[address-0x10000];
+            return this.programHeap[address - 0x10000];
 
-    }else{
-        throw 'out of range exception';
+        } else {
+            throw 'out of range exception';
+        }
+    }
+
+    locate(program) {
+        for (let i = 0; i < program.length; i++) {
+            this.programText[this.programCount] = program[i];
+            this.programCount++;
+        }
+    }
+
+    fetch(program_count) {
+        return this.programText[program_count];
+    }
+
+    store(address, data) {
+        this.programHeap[address - 0x10000] = data;
+    }
+
+    load(address) {
+        return this.programHeap[address - 0x10000];
     }
 }
 
-function locate(program){
-    for(let i = 0; i < program.length; i++){
-        programText[prgCnt] = program[i];
-        prgCnt++;
-    }
-}
-
-function fetch(program_count){
-    return programText[program_count];
-}
-
-function store(address, data){
-    programHeap[address-0x10000] = data;
-}
-
-function load(address){
-    return programHeap[address-0x10000];
-}
-
-
-// console.log(fetch(prgCnt-1));nn
-
-function testMemory(){
+const test = () =>{
+    const testMemory = new Memory();
+    const arr = [0x0000, 0x0001, 0x0002, 0x0003];
     
-    let arr = new Array();
-    arr = [0x0000, 0x0001, 0x0002,0x0003];
+    testMemory.locate(arr);
+    testMemory.store(0x10000, 0xFFFF);
+    console.log(testMemory.fetch(testMemory.programCount - 1));
+    console.log(testMemory.load(0x10000))
     
-    locate(arr);
-    store(0x10000, 0xFFFF);
-    console.log(fetch(prgCnt-1));
-    console.log(load(0x10000))
-
-    console.log(peek(0x0003));
-    console.log(peek(0x10000));
-
+    console.log(testMemory.peek(0x0003));
+    console.log(testMemory.peek(0x10000));
 }
 
-testMemory();
+// test();
+
+module.exports = Memory;
 
